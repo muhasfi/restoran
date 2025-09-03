df010d627dcff3ca5a64c9a5eec9e18a3b390175
muhharis99 <muhammadharischaidir90@gmail.com>
2025-09-03 16:09:36 +0700

    Add factories and seeders for items, categories, roles, and users

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            [
+                'category_name' => 'makanan',
+                'description' => 'All food items',
+            ],
+            [
+                'category_name' => 'minuman',
+                'description' => 'All drinks and beverages',
+            ],
+        ];
+        DB::table('categories')->insert($categories);
+    }
+}
